bb753694fcf8bf0903750b430fd7ffa0fd029e0a
ADSM <yesform2018@gmail.com>
2022-05-22 17:44:11 +0600

    MongoDB added

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q7roj.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async () => {
+
+    try {
+        await client.connect();
+        const productCollenction = client.db("digitaz").collection("products");
+
+
+    }
+    catch {
+        // client.close();
+    }
+}
+run().catch(console.dir);
+
 
 app.listen(port, () => {
     console.log('Digitaz server is running in port', port);
