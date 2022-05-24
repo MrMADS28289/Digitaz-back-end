9bc789e3be9e9092317310f0957d7b3aa898e642
ADSM <yesform2018@gmail.com>
2022-05-24 19:41:10 +0600

    heroku added

--- index.js
+++ index.js
@@ -110,6 +110,13 @@ const run = async () => {
             res.send(result);
         });
 
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollenction.findOne({ email: email });
+            const isAdmin = user?.role === 'admin';
+            res.send({ admin: isAdmin })
+        })
+
         app.post('/review', verifyJWT, async (req, res) => {
             const review = req.body;
             const result = await reviewCollenction.insertOne(review);
