d747f8dd6c1608197d0c19771db7b4992401327f
ADSM <yesform2018@gmail.com>
2022-05-26 03:55:05 +0600

    Add product method completed

--- index.js
+++ index.js
@@ -189,6 +189,12 @@ const run = async () => {
             res.send(result);
         })
 
+        app.post('/product', verifyJWT, async (req, res) => {
+            const product = req.body;
+            const result = await productCollenction.insertOne(product);
+            res.send(result);
+        });
+
         app.put('/product/:id', async (req, res) => {
             const id = req.params.id;
             const stock = req.body;
